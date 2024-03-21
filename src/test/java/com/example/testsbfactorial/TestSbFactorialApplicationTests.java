a7c2fe91718e8a0590cee1b486f9369f6cbdfc9b
JojoPanamo <117886169+JojoPanamo@users.noreply.github.com>
2024-03-21 08:02:20 +0300

    Initial commit

---
+++ src/test/java/com/example/testsbfactorial/TestSbFactorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testsbfactorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSbFactorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
