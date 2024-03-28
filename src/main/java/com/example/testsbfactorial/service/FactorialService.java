595f6fe48a51222f6dbefc7b4d3456fbe3c3560c
JojoPanamo <117886169+JojoPanamo@users.noreply.github.com>
2024-03-28 18:53:59 +0300

    Update FactorialService.java

--- src/main/java/com/example/testsbfactorial/service/FactorialService.java
+++ src/main/java/com/example/testsbfactorial/service/FactorialService.java
@@ -20,16 +20,35 @@ public class FactorialService {
         return new FactorialResponse(result);
     }
 
+//    Реализация расчета факториала с использованием рекурсии
+
+//    protected long calculateFactorial(long num) {
+//        if (num < 0) {
+//            throw new IllegalArgumentException("число должно быть больше либо равно 0");
+//        } else if (num >= 64) {
+//            // для решения проблемы вычисления факториала числа больше 63 можно воспользоваться BigInteger
+//            throw new IllegalArgumentException("число слишком большое. Максимально допустипое значение: 63");
+//        } else if (num == 0) {
+//            return 1;
+//        } else {
+//            return num * calculateFactorial(num - 1);
+//        }
+//    }
+
+
+//    Реализация расчета факториала с использованием цикла
     protected long calculateFactorial(long num) {
         if (num < 0) {
             throw new IllegalArgumentException("число должно быть больше либо равно 0");
-        } else if (num >= 64) {
+        }if (num >= 64) {
             // для решения проблемы вычисления факториала числа больше 63 можно воспользоваться BigInteger
             throw new IllegalArgumentException("число слишком большое. Максимально допустипое значение: 63");
-        } else if (num == 0) {
-            return 1;
         } else {
-            return num * calculateFactorial(num - 1);
+            long result = 1; // начальное значение результата
+            for (long i = 2; i <= num; i++) {
+                result *= i; // умножаем текущий результат на следующее число
+            }
+            return result;
         }
     }
 }
